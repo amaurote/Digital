f70df0558c53a4d79f65bdfe95d49e6a9ed7906a
TBORIK <TBORIK@TSSKKEWKS7650.sk-kosice.t-systems.sk>
2016-11-16 16:30:12 +0100

    office commit

--- src/digital/components/parts/Wire.java
+++ src/digital/components/parts/Wire.java
@@ -1,9 +1,39 @@
 package digital.components.parts;
 
+import java.awt.Graphics;
+
 /**
  *
  * @author AMAUROTE
  */
 public class Wire {
+    // from
+    private int outComponentId;
+    private int outPortId;
     
+    // to
+    private int inComponentId;
+    private int inPortId;
+            
+    // apperance
+    private boolean wrapped;
+    
+    // TODO xy position while moving... and which end
+    
+    public Wire(int outComponentId, int outPortId, int inComponentId, int inPortId) {
+        this.outComponentId = outComponentId;
+        this.outPortId = outPortId;
+        this.inComponentId = inComponentId;
+        this.inPortId = inPortId;
+        
+        this.wrapped = false; //TODO
+    }
+    
+    public void update() {
+        
+    }
+    
+    public void render(Graphics g) {
+        
+    }
 }
