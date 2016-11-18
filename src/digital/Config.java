f083ab172983c242692c8ae702aab37142926397
TBORIK <TBORIK@TSSKKEWKS7650.sk-kosice.t-systems.sk>
2016-11-18 12:09:00 +0100

    office commit

--- src/digital/Config.java
+++ src/digital/Config.java
@@ -7,9 +7,6 @@ package digital;
 public class Config {
     public static final String FRAME_TITLE = "Digital";
     
-    public static final int CANVAS_WIDTH = 400;
-    public static final int CANVAS_HEIGHT = 400;
-    
     public static final int GRID_SIZE = 5;
     
     public static final int FPS = 120;
